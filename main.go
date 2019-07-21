64efdd33720669422d81438389349bd0c7d58202
Henrique Vieira e Sousa <henviso@gmail.com>
2019-07-21 00:58:17 -0300

    Initial commit.

---
+++ main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+import "./mult"
+
+func main() {
+	fmt.Println("2 * 8 is", mult.Mult(2, 8))
+}
---
+++ mult/mult.go
@@ -0,0 +1,12 @@
+package mult
+
+import "../../sum/sum"
+
+func Mult(a int, b int) int {
+	ans := a
+	for i := 0; i < b-1; i++ {
+		ans = sum.Sum(ans, a)
+	}
+
+	return ans;
+}
